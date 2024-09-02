b2551070a9b1bc8c32b89ee9ad8c1133bd897b35
ZeeCapp <ligac.jan@volny.cz>
2024-09-03 00:12:00 +0200

    Working DB connection

--- backend/main.go
+++ backend/main.go
@@ -1,20 +1,47 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
-	"github.com/go-pg/pg"
+	"github.com/jackc/pgx/v5"
 )
 
 func main() {
-	db := pg.Connect(&pg.Options{})
+	dbConnString := os.Getenv("DB_CONN_STRING")
 
-	db.Begin()
+	conn, err := pgx.Connect(context.Background(), dbConnString)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+		os.Exit(1)
+	}
+	defer conn.Close(context.Background())
+
+	var greeting string
+	err = conn.QueryRow(context.Background(), "select 'Hello, world!'").Scan(&greeting)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
+		os.Exit(1)
+	}
 
 	api := gin.Default()
 
-	api.GET("/helloworld", func(ctx *gin.Context) {
+	api.GET("/ping", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{
+			"message": greeting,
+		})
+	})
+
+	api.GET("/", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+
+	api.POST("/helloworld", func(ctx *gin.Context) {
 		ctx.JSON(http.StatusOK, gin.H{
 			"message": "pong",
 		})
