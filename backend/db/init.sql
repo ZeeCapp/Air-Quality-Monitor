90c28888497cb61c7ee2439547a19be766a18826
ZeeCapp <ligac.jan@volny.cz>
2024-09-10 21:13:17 +0200

    functional api with ep to store measurments

--- backend/db/init.sql
+++ backend/db/init.sql
@@ -1,5 +1,5 @@
 create table measurments(
-	id int PRIMARY KEY,
+	id serial PRIMARY KEY,
 	device_uuid varchar(255) NULL,
 	measured_datetime timestamp NOT NULL,
     measured_value varchar(255)
--- backend/main.go
+++ backend/main.go
@@ -5,10 +5,18 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/jackc/pgx/v5"
 )
+
+type measurment struct {
+	Id               int64     `json:"id"`
+	DeviceUUID       string    `json:"deviceUUID"`
+	MeasuredDateTime time.Time `json:"measuredDateTime"`
+	MeasuredValue    string    `json:"measuredValue"`
+}
 
 func main() {
 	dbConnString := os.Getenv("DB_CONN_STRING")
@@ -20,30 +28,49 @@ func main() {
 	}
 	defer conn.Close(context.Background())
 
-	var greeting string
-	err = conn.QueryRow(context.Background(), "select 'Hello, world!'").Scan(&greeting)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
-		os.Exit(1)
-	}
-
 	api := gin.Default()
 
 	api.GET("/ping", func(ctx *gin.Context) {
+		var greeting string
+
+		err = conn.QueryRow(context.Background(), "select 'pong'").Scan(&greeting)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
+			os.Exit(1)
+		}
+
 		ctx.JSON(http.StatusOK, gin.H{
 			"message": greeting,
 		})
 	})
 
-	api.GET("/", func(ctx *gin.Context) {
-		ctx.JSON(http.StatusOK, gin.H{
-			"message": "pong",
-		})
-	})
+	api.POST("/measurment", func(ctx *gin.Context) {
+		var measurment measurment
 
-	api.POST("/helloworld", func(ctx *gin.Context) {
+		if err := ctx.BindJSON(&measurment); err != nil {
+			ctx.JSON(http.StatusInternalServerError, gin.H{
+				"err": err.Error(),
+			})
+		}
+
+		measurment.MeasuredDateTime = time.Now()
+
+		var id int64
+
+		err := conn.QueryRow(
+			context.Background(),
+			"insert into measurments(device_uuid , measured_datetime, measured_value) values ($1, $2, $3) returning id;",
+			measurment.DeviceUUID, measurment.MeasuredDateTime, measurment.MeasuredValue,
+		).Scan(&id)
+
+		if err != nil {
+			ctx.JSON(http.StatusInternalServerError, gin.H{
+				"err": err.Error(),
+			})
+		}
+
 		ctx.JSON(http.StatusOK, gin.H{
-			"message": "pong",
+			"id": id,
 		})
 	})
 
